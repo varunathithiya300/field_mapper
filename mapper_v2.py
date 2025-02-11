c0a1d4b573be5af86ced6e65ce108a3a9f4df6f3
Varun Athithiya <varunathithiya.shenbagaraj@octavebio.com>
2025-02-11 12:31:04 +0530

    added new features

--- mapper_v2.py
+++ mapper_v2.py
@@ -1,9 +1,9 @@
-from requirement_1.r1_methods import run_similarity_analysis
+from requirement_1.r1_methods_v2 import run_similarity_analysis
 
 # File paths
 source_path = r"D:\appathon_personal\field_mapper\requirement_1\vendor_input_format.csv"
 target_path = r"D:\appathon_personal\field_mapper\requirement_1\customer_standard_format.csv"
-output_file = r"mappings.csv"
+output_file = r"mappings_v2.csv"
 model_name = "all-MiniLM-L6-v2"
 
 # Model choice - Change this to any SentenceTransformer model
@@ -11,4 +11,4 @@ model_name = "paraphrase-MiniLM-L6-v2"
 # mpnet = "all-mpnet-base-v2"
 
 # Run the analysis
-run_similarity_analysis(source_path, target_path, model_name, output_file)
+run_similarity_analysis(source_path, target_path, model_name, output_file, filter_highest_only=True)
